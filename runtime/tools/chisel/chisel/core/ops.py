76b8b3978ca9a38c45f5cfda479b00168811e513
Nikola Drakulic <ndrakulic@tenstorrent.com>
2025-06-03 14:20:03 +0000

    WIP

--- runtime/tools/chisel/chisel/core/ops.py
+++ runtime/tools/chisel/chisel/core/ops.py
@@ -1,59 +1,32 @@
 # SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
 #
 # SPDX-License-Identifier: Apache-2.0
+from functools import cache
 from collections import defaultdict
 from typing import List
-from ttmlir.ir import Operation
+
+from ttmlir.ir import Operation, WalkOrder, WalkResult, Context, Module
 from ttmlir.dialects import func
-from ttmlir.ir import Context, Module
 
 from .enums import ExecutionType, Status
+from .tensors import TensorDescriptor, get_op_inputs, get_op_outputs
 
 
-class IRModule:
-    def __init__(self, mlir_text: str, context: Context):
-        self.mlir_module = Module.parse(mlir_text, context)
-        self.context = context
-        self._main_op: Operation | None = None
+class Op:
+    def __init__(self, op: Operation, execution_type: ExecutionType):
+        self.inputs: List[TensorDescriptor] = get_op_inputs(op, execution_type)
+        self.outputs: List[TensorDescriptor] = get_op_outputs(op, execution_type)
+        self.name = op.name
+        self.location = op.location
+        self.asm = op.get_asm()
+        self.execution_type = execution_type
+        self.ir_op = op
+        self.args = None
 
-    def set_main_op(self, name: str):
-        for op in self._dfs(self.mlir_module.operation):
-            if isinstance(op, func.FuncOp) and op.name.value == name:
-                self._main_op = op
-                return
 
-    def get_main_op(self):
-        return self._main_op
-
-    def dfs(self, op: Operation | None = None) -> List[Operation]:
-        if op is None:
-            op = self._main_op
-            assert op is not None
-        ops = []
-        for region in op.regions:
-            for block in region.blocks:
-                for op in block.operations:
-                    if op.name == "ttir.empty":
-                        continue
-                    ops.append(op.opview)
-        return ops
-
-    def _dfs(
-        self, op: Operation | None = None, walk_order: WalkOrder = WalkOrder.POST_ORDER
-    ):
-        if op is None:
-            op = self._main_op
-            assert op is not None
-        ops = []
-
-        def _walk_ops(op):
-            nonlocal ops
-            if not op.name == "ttir.empty":
-                ops.append(op.opview)
-            return WalkResult.ADVANCE
-
-        op.operation.walk(_walk_ops, walk_order=walk_order)
-        return ops
+@cache
+def get_op(op: Operation, execution_type: ExecutionType):
+    return Op(op, execution_type)
 
 
 class OpGroup:
@@ -61,9 +34,6 @@ class OpGroup:
         self.id = id
         self.ops = defaultdict(list)
         self.status = Status.PENDING
-
-    def __setitem__(self, key: ExecutionType, value: Operation):
-        self.ops[key].append(value)
 
     def __getitem__(self, key: ExecutionType):
         return self.ops[key]
@@ -81,3 +51,57 @@ class OpGroup:
                     return op
         else:
             return self.ops[kind][-1]
+
+
+class IRModule:
+    def __init__(self, mlir_text: str, context: Context, execution_type: ExecutionType):
+        self.mlir_module = Module.parse(mlir_text, context)
+        self.context = context
+        self._main_op: Operation | None = None
+        self.op_groups = {}
+        self.execution_type = execution_type
+
+    def set_main_op(self, name: str):
+        for op in self._dfs(self.mlir_module.operation):
+            if isinstance(op, func.FuncOp) and op.name.value == name:
+                self._main_op = op
+                return
+
+    def get_main_op(self):
+        return self._main_op
+
+    def main_body_ops(self, op: Operation | None = None) -> List[Operation]:
+        if op is None:
+            op = self._main_op
+            assert op is not None
+        ops = []
+        for region in op.regions:
+            for block in region.blocks:
+                for op in block.operations:
+                    if op.name == "ttir.empty":
+                        continue
+                    ops.append(get_op(op, self.execution_type))
+        return ops
+
+    def create_op_groups(self):
+        for op in self.main_body_ops():
+            if op.location_hash not in self.op_groups:
+                self.op_groups[op.location_hash] = OpGroup(op.location_hash)
+            self.op_groups[op.location_hash].append(op)
+
+    def _dfs(
+        self, op: Operation | None = None, walk_order: WalkOrder = WalkOrder.POST_ORDER
+    ):
+        if op is None:
+            op = self._main_op
+            assert op is not None
+        ops = []
+
+        def _walk_ops(op):
+            nonlocal ops
+            if not op.name == "ttir.empty":
+                ops.append(op.opview)
+            return WalkResult.ADVANCE
+
+        op.operation.walk(_walk_ops, walk_order=walk_order)
+        return ops
--- runtime/tools/chisel/chisel/core/tensors.py
+++ runtime/tools/chisel/chisel/core/tensors.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 from dataclasses import dataclass
 from functools import cache
-from typing import Tuple
+from typing import List, Tuple
 
 from ttmlir.ir import Operation
 from ttmlir.ir import BlockArgument, Value, Type
@@ -15,9 +15,21 @@ from ..utils.location import hash_location, parse_op_location
 @dataclass
 class TensorDescriptor:
     name: str
-    type: Type
-    location_hash: Tuple[int, int] = None
-    arg: BlockArgument | Value = None
+    dtype: str
+    shape: List[int]
+    location_hash: Tuple[int, int]
+    execution_type: ExecutionType
+
+
+@cache
+def get_tensor_descriptor(op: Operation, execution_type: ExecutionType):
+    return TensorDescriptor(
+        name=op.get_name(),
+        dtype=op.type.element_type.name,
+        shape=op.type.shape,
+        location_hash=hash_location(op.location),
+        execution_type=execution_type,
+    )
 
 
 class TensorValue:
@@ -30,47 +42,26 @@ class TensorValue:
         return f"TensorValue({self.name})"
 
 
-def get_op_outputs(op: Operation):
+def get_op_outputs(op: Operation, execution_type: ExecutionType):
     outputs = []
     for result in op.results:
         if hasattr(result.type, "shape") and hasattr(result.type, "element_type"):
-            outputs.append(
-                TensorDescriptor(
-                    name=result.get_name(),
-                    type=result.type,
-                    location_hash=hash_location(result.location),
-                    arg=result,
-                )
-            )
+            outputs.append(get_tensor_descriptor(result, execution_type))
     return outputs
 
 
-def get_op_inputs(op: Operation):
+def get_op_inputs(op: Operation, execution_type: ExecutionType):
     inputs = []
     for operand in op.operands:
         if hasattr(operand.type, "shape") and hasattr(operand.type, "element_type"):
-            inputs.append(
-                TensorDescriptor(
-                    name=operand.get_name(),
-                    type=operand.type,
-                    location_hash=hash_location(operand.location),
-                    arg=operand,
-                )
-            )
+            inputs.append(get_tensor_descriptor(operand, execution_type))
     return inputs
 
 
-def get_function_inputs(op: Operation):
+def get_function_inputs(op: Operation, execution_type: ExecutionType):
     inputs = []
     for arg in op.arguments:
-        inputs.append(
-            TensorDescriptor(
-                name=arg.get_name(),
-                type=arg.type,
-                location_hash=hash_location(arg.location),
-                arg=arg,
-            )
-        )
+        inputs.append(get_tensor_descriptor(arg, execution_type))
     return inputs
 
 
